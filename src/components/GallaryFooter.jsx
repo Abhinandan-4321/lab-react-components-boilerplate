1baf05de2d629cdbc34f28ddacaf0ea357ec6671
Abhinandan-4321 <abhinandangupta1105@gmail.com>
2024-01-13 10:54:52 +0530

    first

--- src/components/GallaryFooter.jsx
+++ src/components/GallaryFooter.jsx
@@ -1,7 +1,7 @@
-export default function(){
+export default function(props){
   return(
     <footer className='footer-content'>
-      <h3>Author: Kalvium</h3>
+      <h3>Author: {props.name}</h3>
     </footer>
   )
 }
